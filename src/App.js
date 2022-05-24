6a9098985af034a08bf4e4c9db933a7410ac1176
Louise Klingen <l.m.klingen@gmail.com>
2022-05-24 15:57:54 +0200

    ini react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="maintitle">
+          <div>.test app.</div>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='header-item'>examples</div>
+      <div className='header-item'>playing around</div>
+    </div>
+  )
+}
+
+
+
+export default Header;
